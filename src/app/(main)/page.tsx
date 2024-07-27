a35455342f4697bbb5f88434b2abeff88760b784
Amit Aggrawal <amitaggrawal95@gmail.com>
2024-07-27 13:12:22 +0530

    Added Trending topics, and People to follow. Follo btn needs workgin

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,3 +1,4 @@
+import TrendsSidebar from "@/components/TrendsSidebar";
 import Post from "@/components/posts/Post";
 import PostEditor from "@/components/posts/editor/PostEditor";
 import prisma from "@/lib/prisma";
@@ -9,13 +10,14 @@ export default async function Home() {
     orderBy: { createdAt: "desc" },
   });
   return (
-    <main className="w-full min-w-0">
+    <main className="flex w-full min-w-0 gap-5">
       <div className="w-full min-w-0 space-y-5">
         <PostEditor />
         {posts.map((post) => (
           <Post key={post.id} post={post} />
         ))}
       </div>
+      <TrendsSidebar />
     </main>
   );
 }
